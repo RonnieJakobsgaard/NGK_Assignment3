00f4ec0d6ea263986b3fc78b12dd17952e230ad3
Andreas Ventzel <au280318@uni.au.dk>
2019-12-13 10:47:31 +0100

    added counter

---
+++ WeatherStation.Web.Api/Models/counter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherStation.Web.Api.Models
+{
+    public class counter
+    {
+        long value = 0;
+
+        public long Inc()
+        {
+            return ++value;
+        }
+    }
+}
