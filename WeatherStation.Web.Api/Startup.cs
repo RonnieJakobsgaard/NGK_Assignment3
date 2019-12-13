9bac1cf3ad7901f15812dc89180400aca6051ea2
Andreas Ventzel <au280318@uni.au.dk>
2019-12-13 12:59:44 +0100

    Pushing

--- WeatherStation.Web.Api/Startup.cs
+++ WeatherStation.Web.Api/Startup.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,7 +44,8 @@ namespace WeatherStation.Web.Api
 
             services.AddCors();
             services.AddMvc();
-            
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0); // Er denne nødvendig??
             services.AddSignalR();
             services.AddSingleton<counter>();
 
