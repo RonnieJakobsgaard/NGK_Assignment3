4bf3ebfcc85f714d5a6580417116e20f6f9cb57d
Ronnie Jakobsgaard <ronnie192@gmail.com>
2019-12-13 13:45:25 +0100

    SearchOk

---
+++ WeatherStation.Web.Api/Models/MeasurementModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherStation.Web.Api.Models
+{
+    public class MeasurementModel
+    {
+        [BindProperty]
+        public Place Location { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public float AirPressure { get; set; }
+    }
+
+    public class Place
+    {
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
